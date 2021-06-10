e430e2f88b1ff0a632489eb0c04520c0bb2df83b
Paulus Schoutsen <balloob@gmail.com>
2021-06-10 21:04:52 +0000

    Style

--- src/esp_loader.ts
+++ src/esp_loader.ts
@@ -773,7 +773,7 @@ export class ESPLoader extends EventTarget {
     }
     await this.port.writable!.getWriter().close();
     await this.port.close();
-    this.connected = false
+    this.connected = false;
   }
 }
 
